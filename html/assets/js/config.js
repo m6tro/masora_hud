0cb00c3bb096ba87c172cbd286fe721c1df2dce4
metro <151051600+m6tro@users.noreply.github.com>
2023-12-25 04:04:32 +0100

    design and javascript
    😉🏋️‍♀️
    
    Signed-off-by: metro <151051600+m6tro@users.noreply.github.com>

---
+++ html/assets/js/config.js
@@ -0,0 +1,23 @@
+const Config = {
+    ServerName: 'MASORA',
+    ServerType: 'Roleplay',
+
+    FoodHud: true,
+    DrinkHud: true,
+
+    VoicerangeColor: 'rgba(255, 255, 255, 0.25)',
+    SelectedVoicerangeColor: 'rgba(0, 168, 255, 0.75)',
+
+    TalkingColor: 'rgba(255, 255, 255, 0.25)',
+    CurrentTalkingColor: 'rgba(0, 168, 255, 0.75)',
+
+    UnemployedJobLabel: 'Arbeitslos',
+
+    AnnounceLine1: 'ANNOUNCE',
+    AnnounceLine2: 'Masora',
+
+    ProgressbarLine1: 'LOADING',
+    ProgressbarLine2: 'Bar',
+
+    NotifyLine1: 'NOTIFY',
+}
